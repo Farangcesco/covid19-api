c8e2b66c78b88deda7fb01050d062ae9d46fd049
Francesco Diomaiuta <francescodiomaiuta@Francescos-MBP.true.true>
2020-03-22 20:47:24 +0700

    changed orders

--- monitor_api.py
+++ monitor_api.py
@@ -5,27 +5,6 @@ country = str(input('What country do you want to look up? ').title())
 print_all_country = input('Print all countries? yes/no ').lower()
 url_county_name = 'https://coronavirus-19-api.herokuapp.com/countries/' + country
 
-#World stats
-url_all = 'https://coronavirus-19-api.herokuapp.com/all'
-all_data = requests.get(url_all)
-all_data_json = all_data.json()
-
-w_cases = all_data_json['cases']
-w_deaths = all_data_json['deaths']
-w_recovered = all_data_json['recovered']
-w_fatality_rate = (w_deaths/w_cases)*100
-w_r_fatality_rate = round(w_fatality_rate, 2)
-
-print('*'*8)
-print('*'*8)
-print('World Stats\n')
-print('Total Cases: ' + str(f'{w_cases:,}'))
-print('Total Deaths: ' + str(f'{w_deaths:,}'))
-print('Total Recovered: ' + str(f'{w_recovered:,}'))
-print('Global Fatality Rate: ' + str(f'{w_r_fatality_rate:,}') + '%')
-print('*'*8)
-print('*'*8)
-
 #print specific country
 country_data = requests.get(url_county_name)
 country_data_json = country_data.json()
@@ -75,6 +54,26 @@ countries_json = countries_data.json()
 
 #print stuff
 if print_all_country == 'yes':
+        #World stats
+        url_all = 'https://coronavirus-19-api.herokuapp.com/all'
+        all_data = requests.get(url_all)
+        all_data_json = all_data.json()
+        
+        w_cases = all_data_json['cases']
+        w_deaths = all_data_json['deaths']
+        w_recovered = all_data_json['recovered']
+        w_fatality_rate = (w_deaths/w_cases)*100
+        w_r_fatality_rate = round(w_fatality_rate, 2)
+        
+        print('*'*8)
+        print('*'*8)
+        print('World Stats\n')
+        print('Total Cases: ' + str(f'{w_cases:,}'))
+        print('Total Deaths: ' + str(f'{w_deaths:,}'))
+        print('Total Recovered: ' + str(f'{w_recovered:,}'))
+        print('Global Fatality Rate: ' + str(f'{w_r_fatality_rate:,}') + '%')
+        print('*'*8)
+        print('*'*8)
 
         def get_variable2(data_json, variable):
                 return data_json[variable]
